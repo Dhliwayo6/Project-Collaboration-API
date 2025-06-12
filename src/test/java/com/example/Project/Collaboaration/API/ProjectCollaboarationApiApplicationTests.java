24550264be60c1106bcc32383828ab5bbba30c51
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-06-12 15:34:48 +0200

    first commit

---
+++ src/test/java/com/example/Project/Collaboaration/API/ProjectCollaboarationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project.Collaboaration.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCollaboarationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
